be88dac60086adc14541c788b13421e9ff441c03
biswmay <sipu.biswal641@gmail.com>
2023-07-28 18:32:01 +0530

    my first commit

---
+++ src/main/java/com/example/basic_crud_ops/BasicCrudOpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.basic_crud_ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCrudOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCrudOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/basic_crud_ops/BasicCrudOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basic_crud_ops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrudOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
